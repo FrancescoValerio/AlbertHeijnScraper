2eb2de88647d6a39452dddd7dfc88af7c86f0648
Francesco Vaglienti <f.vaglienti@icloud.com>
2021-04-20 18:07:40 +0200

    turned search into function and added auto webdriver

--- albertheijn_scraper.py
+++ albertheijn_scraper.py
@@ -1,15 +1,25 @@
 # %%
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
-browser = webdriver.Chrome()
+# %%
+def check_if_id_exists(id):
+    found = True
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    url = 'https://www.ah.nl/producten/product/' + str(id)
+    driver.get(url)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'accept-cookies'))).click()
+    product_not_found = 'helaas, we konden dit product niet vinden'
+    if product_not_found in str(driver.page_source):
+        found = False
+    driver.quit()
+    return found
 
-test_url = 'https://www.ah.nl/producten/product/wi368'
-
-browser.get(test_url)
-# accept cookies button
-
-
-product_not_found = 'helaas, we konden dit product niet vinden'
-if product_not_found in str(browser.page_source):
-    print('product not found')
-#%%
+test1 = check_if_id_exists('wi368')
+test2 = check_if_id_exists('wi55960')
+print(test1)
+# %%
