bb3c883062f176292874fbc5ceb62a72d7fe2fe1
Francesco Vaglienti <f.vaglienti@icloud.com>
2021-04-20 22:44:47 +0200

    started id scraper

--- albertheijn_scraper.py
+++ albertheijn_scraper.py
@@ -4,22 +4,45 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # %%
-def check_if_id_exists(id):
-    found = True
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+def return_ah_page_source(id):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    valid_page = False
+
+
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     url = 'https://www.ah.nl/producten/product/' + str(id)
     driver.get(url)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'accept-cookies'))).click()
-    product_not_found = 'helaas, we konden dit product niet vinden'
-    if product_not_found in str(driver.page_source):
-        found = False
+    page_source = driver.page_source
+    try:
+        driver.find_element_by_xpath("//h2[contains(text(), 'Voedingswaarden')]").text
+        valid_page = True
+    except NoSuchElementException:
+        pass
     driver.quit()
-    return found
+    return valid_page, id
 
-test1 = check_if_id_exists('wi368')
-test2 = check_if_id_exists('wi55960')
-print(test1)
+threads = []
+valid_ids = []
+with ThreadPoolExecutor(max_workers=45) as executor:
+
+    for x in range(9999999):
+        threads.append(executor.submit(return_ah_page_source, x))
+
+    for task in as_completed(threads):
+        result, id = task.result()
+        if result:
+            valid_ids.append(id)
+        print('\r' + str(id), end ='') 
+
+#%%
+
 # %%
